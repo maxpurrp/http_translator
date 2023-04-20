d9b5361ee7564476d1efb4b08e1bba681ba0ba97
maxpurrp <seminm041@gmail.com>
2023-04-20 07:31:47 -0400

    removed unnecessary lines

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from bottle import get, post, request, response,  run # or route
 from GoogleTranslator import GoogleTranslator
 
-class RequestsHandler(GoogleTranslator):
+localhost, port = '92.118.114.138', 8080
+class RequestsHandler():
     def __init__(self, Unset = False, to_lang = None) -> None:
         self.translator = GoogleTranslator()
         self.Unset = Unset
@@ -17,7 +18,6 @@ class RequestsHandler(GoogleTranslator):
         return output_json
     def translate(self, input_json) -> str:
         try:
-            input_json = input_json
             if input_json == {}:
                 raise KeyError("Excpected json as input")
             if self._check_word_of_sentence(input_json['text']) == True:
@@ -25,14 +25,11 @@ class RequestsHandler(GoogleTranslator):
             if self._check_lang(input_json['to_lang']) == False and self.to_lang == None:
                 raise ValueError('Unsupported value format')
             
-            for _ in input_json:
-                if input_json['text'] and self._check_lang(input_json['to_lang']) and self.to_lang == None:
-                    output_json = {"error" : False, "result" : str(self.translator.translate(input_json['text'],to_lang=input_json['to_lang']))}
-                if input_json['text'] and self.to_lang != None:
-                    output_json = {'error' : False, 'result' : str(self.translator.translate(input_json['text'],to_lang=translator.to_lang))}
-                if input_json['text'] and self.to_lang != None and self._check_lang(input_json['to_lang']):
-                    output_json = {"error" : False, "result" : str(self.translator.translate(input_json['text'],to_lang=input_json['to_lang']))}
-                return output_json
+            if self.to_lang != None:
+                output_json = {'error' : False, 'result' : str(self.translator.translate(input_json['text'],to_lang=translator.to_lang))}
+            if self.to_lang != None and self._check_lang(input_json['to_lang']):
+                output_json = {"error" : False, "result" : str(self.translator.translate(input_json['text'],to_lang=input_json['to_lang']))}
+            return output_json
     
         except KeyError:
             output_json = {'error' : True,
@@ -60,20 +57,18 @@ class RequestsHandler(GoogleTranslator):
                     self.Unset = False
                     self.to_lang = None
                     output_json = {'error' : False}
-                    return output_json
 
             if self._check_lang(input_json['lang']) == True:
                 for _ in input_json:
                     if input_json['Unset'] == False:
                         self.Unset = True
                         self.to_lang = input_json['lang']
-                        output_json = {'error' : False}
-                    return output_json       
+                        output_json = {'error' : False}      
             else:
                 output_json = {'error' : True,
                 'description' : 'Unsupported language format'}
                 response.status = 400
-                return output_json
+            return output_json
 
         except KeyError:
             output_json = {'error' : True,
@@ -96,7 +91,6 @@ class RequestsHandler(GoogleTranslator):
         try:
             if self.Unset and self.to_lang:
                 output_json = {'error' : False, 'lang' : self.to_lang}
-                return output_json
             else:
                 output_json = {'error' : False, 'lang' : None}
             return output_json
@@ -140,4 +134,4 @@ def handler():
 @get('/api/v1/default_language')
 def handler():
     return translator.default_language()
-run(host='92.118.114.138', port=8080)
+run(host = localhost, port = port)
